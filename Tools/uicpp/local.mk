3ffc347aeb1d4335dd17c4083f8176e39edb2f17
Falk Rehwagen <frehwagen@bluewaysw.de>
2018-11-17 19:08:03 +0100

    Specific uicpp defined fixed for windows use only.

--- Tools/uicpp/local.mk
+++ Tools/uicpp/local.mk
@@ -2,8 +2,8 @@
 LIBS		= $(.TARGET:H)/compat.lib
 .PATH.lib	: ../compat $(INSTALL_DIR:H)/compat
 
-XCFLAGS = -DGCC_INCLUDE_DIR=\"unused\" \
-	  -DGPLUSPLUS_INCLUDE_DIR=\"unused\"
+XCFLAGS = -dGCC_INCLUDE_DIR="unused" \
+	  -dGPLUSPLUS_INCLUDE_DIR="unused"
 #  	  -w-pro
 
 #include <$(SYSMAKEFILE)>
