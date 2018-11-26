0b66cc4cdbfb7d9d68f81fa09e21a2b5f973a8c8
Falk Rehwagen <frehwagen@bluewaysw.de>
2018-11-26 21:52:25 +0100

    Reworked uicpp Makefile to support linux and win32.

--- Tools/uicpp/local.mk
+++ Tools/uicpp/local.mk
@@ -2,8 +2,14 @@
 LIBS		= $(.TARGET:H)/compat.lib
 .PATH.lib	: ../compat $(INSTALL_DIR:H)/compat
 
+#if defined(linux)
+XCFLAGS = -dGCC_INCLUDE_DIR=\"unused\" \
+	  -dGPLUSPLUS_INCLUDE_DIR=\"unused\"
+#  	  -w-pro
+#else
 XCFLAGS = -dGCC_INCLUDE_DIR="unused" \
 	  -dGPLUSPLUS_INCLUDE_DIR="unused"
 #  	  -w-pro
+#endif
 
 #include <$(SYSMAKEFILE)>
