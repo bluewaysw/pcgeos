b9a94d3e490d033767bf29ddfd25fdbf408a7004
Michael Casadevall <michael@casadevall.pro>
2018-12-27 05:17:44 -0500

    Remove too long copyright string
    Signed-off-by: Michael Casadevall <michael@casadevall.pro>

--- Library/Breadbox/ExtGraph/local.mk
+++ Library/Breadbox/ExtGraph/local.mk
@@ -7,7 +7,7 @@ XGOCFLAGS = -L extgraph
 #XCCOMFLAGS = -WDE
 
 # Set Copyright notice
-XLINKFLAGS = -N \(C\)98\20Breadbox\20Computer\20Company
+#XLINKFLAGS = -N \(C\)98\20Breadbox\20Computer\20Company
 
 
 
