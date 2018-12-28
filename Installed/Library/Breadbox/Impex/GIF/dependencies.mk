884bb75ce1d8b8355fc02d54649c25554f704d0a
Falk Rehwagen <frehwagen@bluewaysw.de>
2018-12-28 19:59:51 +0100

    Make GIF impex library compile and link.

--- Installed/Library/Breadbox/Impex/GIF/dependencies.mk
+++ Installed/Library/Breadbox/Impex/GIF/dependencies.mk
@@ -1,101 +1,90 @@
 API.obj \
-API.eobj: API/APIMANAGER.ASM \
-                STDAPP.DEF GEOS.DEF GEODE.DEF RESOURCE.DEF EC.DEF LMEM.DEF \
-                OBJECT.DEF GRAPHICS.DEF FONTID.DEF FONT.DEF COLOR.DEF \
-                GSTRING.DEF TEXT.DEF CHAR.DEF HEAP.DEF UI.DEF FILE.DEF \
-                VM.DEF WIN.DEF INPUT.DEF HWR.DEF LOCALIZE.DEF SLLANG.DEF \
-                OBJECTS/PROCESSC.DEF OBJECTS/METAC.DEF CHUNKARR.DEF \
-                GEOWORKS.DEF GCNLIST.DEF TIMEDATE.DEF \
-                OBJECTS/TEXT/TCOMMON.DEF STYLESH.DEF IACP.DEF \
-                OBJECTS/UIINPUTC.DEF OBJECTS/VISC.DEF OBJECTS/VCOMPC.DEF \
-                OBJECTS/VCNTC.DEF INTERNAL/VUTILS.DEF OBJECTS/GENC.DEF \
-                DISK.DEF DRIVE.DEF UDIALOG.DEF OBJECTS/GINTERC.DEF \
-                TOKEN.DEF OBJECTS/CLIPBRD.DEF OBJECTS/GSYSC.DEF \
-                OBJECTS/GPROCC.DEF ALB.DEF OBJECTS/GFIELDC.DEF \
-                OBJECTS/GSCREENC.DEF OBJECTS/GFSELC.DEF \
-                OBJECTS/GVIEWC.DEF OBJECTS/GCONTC.DEF OBJECTS/GCTRLC.DEF \
-                OBJECTS/GDOCC.DEF OBJECTS/GDOCCTRL.DEF \
-                OBJECTS/GDOCGRPC.DEF OBJECTS/GEDITCC.DEF \
-                OBJECTS/GVIEWCC.DEF OBJECTS/GTOOLCC.DEF \
-                OBJECTS/GPAGECC.DEF OBJECTS/GPENICC.DEF \
-                OBJECTS/GGLYPHC.DEF OBJECTS/GTRIGC.DEF \
-                OBJECTS/GBOOLGC.DEF OBJECTS/GITEMGC.DEF \
-                OBJECTS/GDLISTC.DEF OBJECTS/GITEMC.DEF OBJECTS/GBOOLC.DEF \
-                OBJECTS/GDISPC.DEF OBJECTS/GDCTRLC.DEF OBJECTS/GPRIMC.DEF \
-                OBJECTS/GAPPC.DEF OBJECTS/GTEXTC.DEF OBJECTS/GGADGETC.DEF \
-                OBJECTS/GVALUEC.DEF OBJECTS/GTOOLGC.DEF \
-                INTERNAL/GUTILS.DEF OBJECTS/HELPCC.DEF OBJECTS/EMENUC.DEF \
-                OBJECTS/EMOMC.DEF OBJECTS/EMTRIGC.DEF INTERNAL/UPROCC.DEF \
-                LIBRARY.DEF MATH.DEF API/GIF.DEF MANAGER.RDEF
-EXPGIF.obj \
-EXPGIF.eobj: STDAPP.GOH OBJECT.GOH UI.GOH OBJECTS/METAC.GOH \
-                OBJECTS/INPUTC.GOH OBJECTS/CLIPBRD.GOH \
-                OBJECTS/UIINPUTC.GOH IACP.GOH OBJECTS/WINC.GOH \
-                OBJECTS/GPROCC.GOH ALB.GOH OBJECTS/PROCESSC.GOH \
-                OBJECTS/VISC.GOH OBJECTS/VCOMPC.GOH OBJECTS/VCNTC.GOH \
-                OBJECTS/GAPPC.GOH OBJECTS/GENC.GOH OBJECTS/GINTERC.GOH \
-                OBJECTS/GPRIMC.GOH OBJECTS/GDISPC.GOH OBJECTS/GTRIGC.GOH \
-                OBJECTS/GVIEWC.GOH OBJECTS/GTEXTC.GOH OBJECTS/VTEXTC.GOH \
-                OBJECTS/GCTRLC.GOH GCNLIST.GOH SPOOL.GOH \
-                OBJECTS/GFSELC.GOH OBJECTS/GGLYPHC.GOH \
-                OBJECTS/GDOCCTRL.GOH OBJECTS/GDOCGRPC.GOH \
-                OBJECTS/GDOCC.GOH OBJECTS/GCONTC.GOH OBJECTS/GDCTRLC.GOH \
-                OBJECTS/GEDITCC.GOH OBJECTS/GBOOLGC.GOH \
-                OBJECTS/GITEMGC.GOH OBJECTS/GDLISTC.GOH \
-                OBJECTS/GITEMC.GOH OBJECTS/GBOOLC.GOH \
-                OBJECTS/GGADGETC.GOH OBJECTS/GTOOLCC.GOH \
-                OBJECTS/GVALUEC.GOH OBJECTS/GTOOLGC.GOH \
-                OBJECTS/HELPCC.GOH IMPEX.GOH EXTGRAPH.GOH GIFLIB.GOH
-EXPGIF.obj \
-EXPGIF.eobj: XLAT/EXPGIF.GOC GEOS.H  \
-                 \
-                 HEAP.H GEODE.H \
-                RESOURCE.H EC.H OBJECT.H LMEM.H GRAPHICS.H FONTID.H \
-                FONT.H COLOR.H GSTRING.H TIMER.H VM.H DBASE.H LOCALIZE.H \
-                ANSI/CTYPE.H TIMEDATE.H FILE.H SLLANG.H SYSTEM.H \
-                GEOWORKS.H CHUNKARR.H OBJECTS/HELPCC.H DISK.H DRIVE.H \
-                INPUT.H CHAR.H HWR.H WIN.H UDIALOG.H OBJECTS/GINTERC.H \
-                OBJECTS/TEXT/TCOMMON.H STYLESH.H DRIVER.H THREAD.H \
-                PRINT.H INTERNAL/SPOOLINT.H SERIALDR.H PARALLDR.H \
-                HUGEARR.H FILEENUM.H ANSI/STDIO.H \
-                 IECOMMON.H XLATLIB.H \
-                ANSI/STRING.H
+API.eobj: API/apiManager.asm \
+                stdapp.def geos.def geode.def resource.def ec.def lmem.def \
+                object.def graphics.def fontID.def font.def color.def \
+                gstring.def text.def char.def heap.def ui.def file.def \
+                vm.def win.def input.def hwr.def localize.def sllang.def \
+                Objects/processC.def Objects/metaC.def chunkarr.def \
+                geoworks.def gcnlist.def timedate.def \
+                Objects/Text/tCommon.def stylesh.def iacp.def \
+                Objects/uiInputC.def Objects/visC.def Objects/vCompC.def \
+                Objects/vCntC.def Internal/vUtils.def Objects/genC.def \
+                disk.def drive.def uDialog.def Objects/gInterC.def \
+                token.def Objects/clipbrd.def Objects/gSysC.def \
+                Objects/gProcC.def alb.def Objects/gFieldC.def \
+                Objects/gScreenC.def Objects/gFSelC.def \
+                Objects/gViewC.def Objects/gContC.def Objects/gCtrlC.def \
+                Objects/gDocC.def Objects/gDocCtrl.def \
+                Objects/gDocGrpC.def Objects/gEditCC.def \
+                Objects/gViewCC.def Objects/gToolCC.def \
+                Objects/gPageCC.def Objects/gPenICC.def \
+                Objects/gGlyphC.def Objects/gTrigC.def \
+                Objects/gBoolGC.def Objects/gItemGC.def \
+                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
+                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
+                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
+                Objects/gValueC.def Objects/gToolGC.def \
+                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
+                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
+                library.def math.def API/gif.def manager.rdef
 IMPGIF.obj \
-IMPGIF.eobj: STDAPP.GOH OBJECT.GOH UI.GOH OBJECTS/METAC.GOH \
-                OBJECTS/INPUTC.GOH OBJECTS/CLIPBRD.GOH \
-                OBJECTS/UIINPUTC.GOH IACP.GOH OBJECTS/WINC.GOH \
-                OBJECTS/GPROCC.GOH ALB.GOH OBJECTS/PROCESSC.GOH \
-                OBJECTS/VISC.GOH OBJECTS/VCOMPC.GOH OBJECTS/VCNTC.GOH \
-                OBJECTS/GAPPC.GOH OBJECTS/GENC.GOH OBJECTS/GINTERC.GOH \
-                OBJECTS/GPRIMC.GOH OBJECTS/GDISPC.GOH OBJECTS/GTRIGC.GOH \
-                OBJECTS/GVIEWC.GOH OBJECTS/GTEXTC.GOH OBJECTS/VTEXTC.GOH \
-                OBJECTS/GCTRLC.GOH GCNLIST.GOH SPOOL.GOH \
-                OBJECTS/GFSELC.GOH OBJECTS/GGLYPHC.GOH \
-                OBJECTS/GDOCCTRL.GOH OBJECTS/GDOCGRPC.GOH \
-                OBJECTS/GDOCC.GOH OBJECTS/GCONTC.GOH OBJECTS/GDCTRLC.GOH \
-                OBJECTS/GEDITCC.GOH OBJECTS/GBOOLGC.GOH \
-                OBJECTS/GITEMGC.GOH OBJECTS/GDLISTC.GOH \
-                OBJECTS/GITEMC.GOH OBJECTS/GBOOLC.GOH \
-                OBJECTS/GGADGETC.GOH OBJECTS/GTOOLCC.GOH \
-                OBJECTS/GVALUEC.GOH OBJECTS/GTOOLGC.GOH \
-                OBJECTS/HELPCC.GOH IMPEX.GOH GIFLIB.GOH
+IMPGIF.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
+                Objects/inputC.goh Objects/clipbrd.goh \
+                Objects/uiInputC.goh iacp.goh Objects/winC.goh \
+                Objects/gProcC.goh alb.goh Objects/processC.goh \
+                Objects/visC.goh Objects/vCompC.goh Objects/vCntC.goh \
+                Objects/gAppC.goh Objects/genC.goh Objects/gInterC.goh \
+                Objects/gPrimC.goh Objects/gDispC.goh Objects/gTrigC.goh \
+                Objects/gViewC.goh Objects/gTextC.goh Objects/vTextC.goh \
+                Objects/gCtrlC.goh gcnlist.goh spool.goh \
+                Objects/gFSelC.goh Objects/gGlyphC.goh \
+                Objects/gDocCtrl.goh Objects/gDocGrpC.goh \
+                Objects/gDocC.goh Objects/gContC.goh Objects/gDCtrlC.goh \
+                Objects/gEditCC.goh Objects/gBoolGC.goh \
+                Objects/gItemGC.goh Objects/gDListC.goh \
+                Objects/gItemC.goh Objects/gBoolC.goh \
+                Objects/gGadgetC.goh Objects/gToolCC.goh \
+                Objects/gValueC.goh Objects/gToolGC.goh \
+                Objects/helpCC.goh impex.goh giflib.goh
 IMPGIF.obj \
-IMPGIF.eobj: XLAT/IMPGIF.GOC GEOS.H  \
-                 \
-                 HEAP.H GEODE.H \
-                RESOURCE.H EC.H OBJECT.H LMEM.H GRAPHICS.H FONTID.H \
-                FONT.H COLOR.H GSTRING.H TIMER.H VM.H DBASE.H LOCALIZE.H \
-                ANSI/CTYPE.H TIMEDATE.H FILE.H SLLANG.H SYSTEM.H \
-                GEOWORKS.H CHUNKARR.H OBJECTS/HELPCC.H DISK.H DRIVE.H \
-                INPUT.H CHAR.H HWR.H WIN.H UDIALOG.H OBJECTS/GINTERC.H \
-                OBJECTS/TEXT/TCOMMON.H STYLESH.H DRIVER.H THREAD.H \
-                PRINT.H INTERNAL/SPOOLINT.H SERIALDR.H PARALLDR.H \
-                HUGEARR.H FILEENUM.H IECOMMON.H XLATLIB.H ANSI/STDIO.H \
-                 ANSI/STRING.H
-IMPGIF2.obj \
-IMPGIF2.eobj: 
-IMPGIF2.obj \
-IMPGIF2.eobj: XLAT/IMPGIF2.GOC
-MANAGER.rdef: GENERIC.UIH PRODUCT.UIH
+IMPGIF.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
+                graphics.h fontID.h font.h color.h gstring.h timer.h vm.h \
+                dbase.h localize.h Ansi/ctype.h timedate.h file.h \
+                sllang.h system.h geoworks.h chunkarr.h Objects/helpCC.h \
+                disk.h drive.h input.h char.h hwr.h win.h uDialog.h \
+                Objects/gInterC.h Objects/Text/tCommon.h stylesh.h \
+                driver.h thread.h print.h Internal/spoolInt.h serialDr.h \
+                parallDr.h hugearr.h fileEnum.h ieCommon.h xlatLib.h \
+                Ansi/stdio.h Ansi/string.h
+EXPGIF.obj \
+EXPGIF.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
+                Objects/inputC.goh Objects/clipbrd.goh \
+                Objects/uiInputC.goh iacp.goh Objects/winC.goh \
+                Objects/gProcC.goh alb.goh Objects/processC.goh \
+                Objects/visC.goh Objects/vCompC.goh Objects/vCntC.goh \
+                Objects/gAppC.goh Objects/genC.goh Objects/gInterC.goh \
+                Objects/gPrimC.goh Objects/gDispC.goh Objects/gTrigC.goh \
+                Objects/gViewC.goh Objects/gTextC.goh Objects/vTextC.goh \
+                Objects/gCtrlC.goh gcnlist.goh spool.goh \
+                Objects/gFSelC.goh Objects/gGlyphC.goh \
+                Objects/gDocCtrl.goh Objects/gDocGrpC.goh \
+                Objects/gDocC.goh Objects/gContC.goh Objects/gDCtrlC.goh \
+                Objects/gEditCC.goh Objects/gBoolGC.goh \
+                Objects/gItemGC.goh Objects/gDListC.goh \
+                Objects/gItemC.goh Objects/gBoolC.goh \
+                Objects/gGadgetC.goh Objects/gToolCC.goh \
+                Objects/gValueC.goh Objects/gToolGC.goh \
+                Objects/helpCC.goh impex.goh extgraph.goh giflib.goh
+EXPGIF.obj \
+EXPGIF.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
+                graphics.h fontID.h font.h color.h gstring.h timer.h vm.h \
+                dbase.h localize.h Ansi/ctype.h timedate.h file.h \
+                sllang.h system.h geoworks.h chunkarr.h Objects/helpCC.h \
+                disk.h drive.h input.h char.h hwr.h win.h uDialog.h \
+                Objects/gInterC.h Objects/Text/tCommon.h stylesh.h \
+                driver.h thread.h print.h Internal/spoolInt.h serialDr.h \
+                parallDr.h hugearr.h fileEnum.h Ansi/stdio.h ieCommon.h \
+                xlatLib.h Ansi/string.h
+manager.rdef: generic.uih product.uih
 
-GIFEC.geo GIF.geo : GEOS.LDF UI.LDF IMPEX.LDF ANSIC.LDF EXTGRAPH.LDF GIFLIB.LDF 
+gifEC.geo gif.geo : geos.ldf ui.ldf impex.ldf ansic.ldf extgraph.ldf giflib.ldf 
---
+++ Library/Breadbox/Impex/GIF/API/apiManager.asm
@@ -0,0 +1,300 @@
+include stdapp.def
+include vm.def
+include Objects/gCtrlC.def
+include library.def
+include resource.def    ; idata/udata, ProcCallFixedOrMovable etc.
+include gstring.def
+
+UseLib math.def
+DefLib API/gif.def
+
+include manager.rdef
+
+; global function implemented in C
+global  GIFIMPORT: far
+global  GIFTESTFILE: far
+global  GIFEXPORT: far
+
+; build up the right palette from gstring
+global  PALGSTRINGCOLELEMENT: far
+global  MY_GRPARSEGSTRING:far
+
+	SetGeosConvention               ; set calling convention
+
+;================================================================================
+
+INIT    segment resource
+	assume cs:INIT
+
+;--------------------------------------------------------------------------------
+
+	db      "OK"
+	dw      InfoResource            ; resource containing format names
+	dw      1                       ; contains two sub-formats
+	dw      4000h                   ; this is a graphics translator
+	db      "OK"
+
+;--------------------------------------------------------------------------------
+
+global  LibraryEntry: far
+
+LibraryEntry proc far
+	clc
+	ret
+LibraryEntry endp
+
+;--------------------------------------------------------------------------------
+
+INIT    ends
+
+;================================================================================
+
+ASM     segment resource
+	assume cs:ASM
+
+global  TransExport: far
+global  TransImport: far
+global  TransGetFormat: far
+global  TransGetImportUI: far
+global  TransGetExportUI: far
+global  TransInitImportUI: far
+global  TransInitExportUI: far
+global  TransGetImportOptions: far
+global  TransGetExportOptions: far
+
+;--------------------------------------------------------------------------------
+
+TransExport proc far
+	uses    es,ds,si,di
+	.enter
+	  push    ds                    ; arg 1: pointer to impex data block
+	  push    si
+
+	  mov     ax,idata              ; DS=dgroup
+	  mov     ds,ax
+	  mov     ax,25                 ; Allocate default float stack size
+	  mov     bl,FLOAT_STACK_GROW
+	  call    FloatInit
+	  call    GIFEXPORT            	; Call high-level procedure to do work
+	  call    FloatExit
+	  mov     bx,dx                 ; BX:AX returns error code or format ID
+	.leave
+	ret
+TransExport endp
+
+;--------------------------------------------------------------------------------
+
+TransImport proc far
+	uses    es,ds,si,di
+_vmc    local   dword
+	.enter
+	  push    ds                    ; arg 1: pointer to impex data block
+	  push    si
+	  push    ss                    ; arg 2: pointer to vm chain return buf
+	  lea     ax,_vmc
+	  push    ax
+
+	  mov     ax,idata              ; DS=dgroup
+	  mov     ds,ax
+	  mov     ax,25                 ; Allocate default float stack size
+	  mov     bl,FLOAT_STACK_GROW
+	  call    FloatInit
+	  call    GIFIMPORT             ; Call high-level procedure to do work
+	  call    FloatExit
+	  mov     bx,dx                 ; BX:AX returns error code or format ID
+	  movdw   dxcx,_vmc             ; VMChain of object returned in DX:CX
+	.leave
+	ret
+TransImport endp
+
+;--------------------------------------------------------------------------------
+
+TransGetFormat proc far
+	uses    es,ds,si,di
+	.enter
+
+	push    si                    ; argument file handle
+
+	mov     ax,idata              ; DS=dgroup
+	mov     ds,ax
+	mov     ax,25                 ; Allocate default float stack size
+	mov     bl,FLOAT_STACK_GROW
+	call    FloatInit
+	call    GIFTESTFILE           ; Call high-level procedure to do work
+	call    FloatExit
+
+	mov     cx, ax
+	xor     ax, ax
+	.leave
+	ret
+
+TransGetFormat endp
+
+;--------------------------------------------------------------------------------
+
+TransGetImportUI proc far
+	mov     bp,00004h                      ; 0006 BD0800
+	push    di                             ; 0009 57
+	push    ds                             ; 000A 1E
+	mov     bx,handle InfoResource         ; 000B BB1400
+	call    MemLock                        ; 000E 9A05000000
+	mov     ds,ax                          ; 0013 8ED8
+	mov     ax,0000Eh                      ; 0015 B80E00
+	mul     cx                             ; 0018 F7E1
+	mov     di,00010h                      ; 001A BF1000
+	add     di,ax                          ; 001D 03F8
+	mov     cx,[ds:bp+di+002h]             ; 001F 3E8B4B02
+	mov     dx,[ds:bp+di]                  ; 0023 3E8B13
+	call    MemUnlock                      ; 0026 9A06000000
+	xor     ax,ax                          ; 002B 33C0
+	mov     bx,ax                          ; 002D 8BD8
+	pop     ds                             ; 002F 1F
+	pop     di                             ; 0030 5F
+	ret
+TransGetImportUI endp
+
+;--------------------------------------------------------------------------------
+
+TransGetExportUI proc far
+	mov     bp,00004h                      ; 0006 BD0800
+	push    di                             ; 0009 57
+	push    ds                             ; 000A 1E
+	mov     bx,handle InfoResource         ; 000B BB1400
+	call    MemLock                        ; 000E 9A05000000
+	mov     ds,ax                          ; 0013 8ED8
+	mov     ax,0000Eh                      ; 0015 B80E00
+	mul     cx                             ; 0018 F7E1
+	mov     di,00014h                      ; 001A BF1000
+	add     di,ax                          ; 001D 03F8
+	mov     cx,[ds:bp+di+002h]             ; 001F 3E8B4B02
+	mov     dx,[ds:bp+di]                  ; 0023 3E8B13
+	call    MemUnlock                      ; 0026 9A06000000
+	xor     ax,ax                          ; 002B 33C0
+	mov     bx,ax                          ; 002D 8BD8
+	pop     ds                             ; 002F 1F
+	pop     di                             ; 0030 5F
+	ret
+TransGetExportUI endp
+
+;--------------------------------------------------------------------------------
+
+TransGetImportOptions proc far
+	push    ax                             ; 01ED 50
+	push    cx                             ; 01EE 51
+	push    bx                             ; 01EF 53
+	push    bp                             ; 01F0 55
+	push    si                             ; 01F1 56
+	push    di                             ; 01F2 57
+	push    ds                             ; 01F3 1E
+
+comment %
+	mov     ax,MSG_GEN_ITEM_GROUP_GET_SELECTION
+	mov     bx,dx
+	mov     si,offset BmpExpBitFormGroup
+	mov     di,mask MF_CALL
+	call    ObjMessage
+
+%
+	push    ax
+	mov     ax,00002h
+	mov     cl,050h
+	mov     ch,040h
+	call    MemAlloc
+	xor     dx,dx
+	jc      iopt_err
+	push    ax
+	pop     ds
+	pop     ax
+	mov     [ds:00000h],ax          ; booleanOptions
+	call    MemUnlock
+	mov     dx,bx
+	clc
+iopt_err:
+	pop     ds                             ; 0236 1F
+	pop     di                             ; 0237 5F
+	pop     si                             ; 0238 5E
+	pop     bp                             ; 0239 5D
+	pop     bx                             ; 023A 5B
+	pop     cx                             ; 023B 59
+	pop     ax                             ; 023C 58 
+	ret
+TransGetImportOptions endp
+
+;--------------------------------------------------------------------------------
+
+TransGetExportOptions proc far
+        push    ax                             ; 01ED 50
+        push    cx                             ; 01EE 51
+        push    bx                             ; 01EF 53
+        push    bp                             ; 01F0 55
+        push    si                             ; 01F1 56
+        push    di                             ; 01F2 57
+        push    ds                             ; 01F3 1E
+
+        mov     ax,MSG_GEN_ITEM_GROUP_GET_SELECTION
+        mov     bx,dx
+        mov     si,offset GifExpFormGroup
+        mov     di,mask MF_CALL
+        call    ObjMessage
+
+        push    ax
+        mov     ax,00002h
+        mov     cl,050h
+        mov     ch,040h
+        call    MemAlloc
+        xor     dx,dx
+        jc      iopt_err
+        push    ax
+        pop     ds
+        pop     ax
+        mov     [ds:00000h],ax          ; booleanOptions
+        call    MemUnlock
+        mov     dx,bx
+        clc
+iopt_err:
+        pop     ds                             ; 0236 1F
+        pop     di                             ; 0237 5F
+        pop     si                             ; 0238 5E
+        pop     bp                             ; 0239 5D
+        pop     bx                             ; 023A 5B
+        pop     cx                             ; 023B 59
+        pop     ax                             ; 023C 58 
+        ret
+TransGetExportOptions endp
+
+;--------------------------------------------------------------------------------
+
+TransInitImportUI proc far
+	ret
+TransInitImportUI endp
+
+;--------------------------------------------------------------------------------
+
+TransInitExportUI proc far
+	ret
+TransInitExportUI endp
+
+ASM     ends
+
+;================================================================================
+
+InfoResource    segment lmem LMEM_TYPE_GENERAL,mask LMF_IN_RESOURCE
+
+		dw      fmt_1_name,fmt_1_mask
+		D_OPTR  0 
+		D_OPTR  GifExportGroup
+		dw      0C000h     ; Currently we only support import
+
+	dw      0
+
+fmt_1_name      chunk   char
+	char    "GIF",0
+fmt_1_name      endc
+
+fmt_1_mask      chunk   char
+	char    "*.gif",0
+fmt_1_mask      endc
+
+InfoResource    ends
+
+;================================================================================
