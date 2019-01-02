b75a055d4a06cf6c02bef821384caef9def1a451
Jirka Kunze <jirkakunze@web.de>
2019-01-02 04:35:03 +0100

    Bounce added

---
+++ Installed/Appl/Bounce/dependencies.mk
@@ -0,0 +1,33 @@
+bounce.rdef: generic.uih product.uih Art/mkrBounce.ui \
+                Art/mkrBounceTiny.ui
+bounce.obj \
+bounce.eobj: geos.def heap.def geode.def resource.def ec.def sem.def \
+                object.def lmem.def win.def graphics.def fontID.def \
+                font.def color.def file.def localize.def sllang.def \
+                timer.def char.def thread.def gstring.def text.def \
+                Objects/winC.def Objects/metaC.def chunkarr.def \
+                geoworks.def ui.def vm.def input.def hwr.def \
+                Objects/processC.def gcnlist.def timedate.def \
+                Objects/Text/tCommon.def stylesh.def iacp.def \
+                Objects/uiInputC.def Objects/visC.def Objects/vCompC.def \
+                Objects/vCntC.def Internal/vUtils.def Objects/genC.def \
+                disk.def drive.def uDialog.def Objects/gInterC.def \
+                token.def Objects/clipbrd.def Objects/gSysC.def \
+                Objects/gProcC.def alb.def Objects/gFieldC.def \
+                Objects/gScreenC.def Objects/gFSelC.def \
+                Objects/gViewC.def Objects/gContC.def Objects/gCtrlC.def \
+                Objects/gDocC.def Objects/gDocCtrl.def \
+                Objects/gDocGrpC.def Objects/gEditCC.def \
+                Objects/gViewCC.def Objects/gToolCC.def \
+                Objects/gPageCC.def Objects/gPenICC.def \
+                Objects/gGlyphC.def Objects/gTrigC.def \
+                Objects/gBoolGC.def Objects/gItemGC.def \
+                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
+                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
+                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
+                Objects/gValueC.def Objects/gToolGC.def \
+                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
+                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
+                bounce.rdef
+
+bounceEC.geo bounce.geo : geos.ldf ui.ldf 
