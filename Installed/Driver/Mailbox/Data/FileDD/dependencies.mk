a12b5fb052cb77be1b02111d464d1aa9879e2ec9
Falk Rehwagen <frehwagen@bluewaysw.de>
2019-02-10 15:18:46 +0100

    Make FileDD driver compile and link.

--- Installed/Driver/Mailbox/Data/FileDD/dependencies.mk
+++ Installed/Driver/Mailbox/Data/FileDD/dependencies.mk
@@ -1,4 +1,34 @@
-FILEDDMANAGER.obj \
-FILEDDMANAGER.eobj: 
+fileddManager.obj \
+fileddManager.eobj: geos.def heap.def geode.def resource.def ec.def lmem.def \
+                driver.def system.def localize.def sllang.def file.def \
+                drive.def disk.def ui.def vm.def text.def fontID.def \
+                graphics.def font.def color.def char.def win.def \
+                input.def hwr.def Objects/processC.def Objects/metaC.def \
+                object.def chunkarr.def geoworks.def gcnlist.def \
+                timedate.def Objects/Text/tCommon.def stylesh.def \
+                iacp.def Objects/uiInputC.def Objects/visC.def \
+                Objects/vCompC.def Objects/vCntC.def Internal/vUtils.def \
+                Objects/genC.def uDialog.def Objects/gInterC.def \
+                token.def Objects/clipbrd.def Objects/gSysC.def \
+                Objects/gProcC.def alb.def Objects/gFieldC.def \
+                Objects/gScreenC.def Objects/gFSelC.def \
+                Objects/gViewC.def Objects/gContC.def Objects/gCtrlC.def \
+                Objects/gDocC.def Objects/gDocCtrl.def \
+                Objects/gDocGrpC.def Objects/gEditCC.def \
+                Objects/gViewCC.def Objects/gToolCC.def \
+                Objects/gPageCC.def Objects/gPenICC.def \
+                Objects/gGlyphC.def Objects/gTrigC.def \
+                Objects/gBoolGC.def Objects/gItemGC.def \
+                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
+                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
+                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
+                Objects/gValueC.def Objects/gToolGC.def \
+                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
+                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
+                mailbox.def medium.def Internal/mbDataDr.def \
+                Mailbox/filedd.def filedd.asm fileddConstant.def \
+                fileddVariable.def fileddEntry.asm fileddRead.asm \
+                fileddWrite.asm fileddCode.asm fileddUtils.asm \
+                fileddBody.asm
 
-FileDDEC.geo FileDD.geo : GEOS.LDF MAILBOX.LDF 
+fileddEC.geo filedd.geo : geos.ldf mailbox.ldf 
