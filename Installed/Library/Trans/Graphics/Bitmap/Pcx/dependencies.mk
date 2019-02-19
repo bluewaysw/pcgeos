fc5304585b73473fdaae5762eb63ba1870c12a55
Falk Rehwagen <frehwagen@bluewaysw.de>
2019-02-19 18:46:33 +0100

    Fix pcx trans library makefiles.

--- Installed/Library/Trans/Graphics/Bitmap/Pcx/dependencies.mk
+++ Installed/Library/Trans/Graphics/Bitmap/Pcx/dependencies.mk
@@ -1,3 +1,38 @@
+Lib.obj \
+Lib.eobj: Lib/libManager.asm \
+                pcxGeode.def graphicsGeode.def geos.def geode.def \
+                Objects/processC.def Objects/metaC.def object.def \
+                lmem.def chunkarr.def geoworks.def file.def library.def \
+                graphics.def fontID.def font.def color.def gstring.def \
+                text.def char.def win.def system.def localize.def \
+                sllang.def resource.def heap.def timedate.def input.def \
+                hwr.def timer.def initfile.def vm.def Objects/winC.def \
+                sem.def ec.def ui.def gcnlist.def \
+                Objects/Text/tCommon.def stylesh.def iacp.def \
+                Objects/uiInputC.def Objects/visC.def Objects/vCompC.def \
+                Objects/vCntC.def Internal/vUtils.def Objects/genC.def \
+                disk.def drive.def uDialog.def Objects/gInterC.def \
+                token.def Objects/clipbrd.def Objects/gSysC.def \
+                Objects/gProcC.def alb.def Objects/gFieldC.def \
+                Objects/gScreenC.def Objects/gFSelC.def \
+                Objects/gViewC.def Objects/gContC.def Objects/gCtrlC.def \
+                Objects/gDocC.def Objects/gDocCtrl.def \
+                Objects/gDocGrpC.def Objects/gEditCC.def \
+                Objects/gViewCC.def Objects/gToolCC.def \
+                Objects/gPageCC.def Objects/gPenICC.def \
+                Objects/gGlyphC.def Objects/gTrigC.def \
+                Objects/gBoolGC.def Objects/gItemGC.def \
+                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
+                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
+                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
+                Objects/gValueC.def Objects/gToolGC.def \
+                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
+                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
+                math.def impex.def ieCommon.def ansic.def \
+                Internal/dib.def Internal/xlatLib.def \
+                transCommonGlobal.def graphicsMacro.def pcxGlobal.def \
+                pcx.def libExport.rdef libFormat.asm libMain.asm \
+                transUI.asm transLibEntry.asm transSemaphore.asm
 Export.obj \
 Export.eobj: Export/exportManager.asm \
                 pcxGeode.def graphicsGeode.def geos.def geode.def \
@@ -66,144 +101,57 @@ Import.eobj: Import/importManager.asm \
                 Internal/dib.def Internal/xlatLib.def \
                 transCommonGlobal.def graphicsMacro.def pcxGlobal.def \
                 pcx.def importCommon.asm importMain.asm
-Lib.obj \
-Lib.eobj: Lib/libManager.asm \
-                pcxGeode.def graphicsGeode.def geos.def geode.def \
-                Objects/processC.def Objects/metaC.def object.def \
-                lmem.def chunkarr.def geoworks.def file.def library.def \
-                graphics.def fontID.def font.def color.def gstring.def \
-                text.def char.def win.def system.def localize.def \
-                sllang.def resource.def heap.def timedate.def input.def \
-                hwr.def timer.def initfile.def vm.def Objects/winC.def \
-                sem.def ec.def ui.def gcnlist.def \
-                Objects/Text/tCommon.def stylesh.def iacp.def \
-                Objects/uiInputC.def Objects/visC.def Objects/vCompC.def \
-                Objects/vCntC.def Internal/vUtils.def Objects/genC.def \
-                disk.def drive.def uDialog.def Objects/gInterC.def \
-                token.def Objects/clipbrd.def Objects/gSysC.def \
-                Objects/gProcC.def alb.def Objects/gFieldC.def \
-                Objects/gScreenC.def Objects/gFSelC.def \
-                Objects/gViewC.def Objects/gContC.def Objects/gCtrlC.def \
-                Objects/gDocC.def Objects/gDocCtrl.def \
-                Objects/gDocGrpC.def Objects/gEditCC.def \
-                Objects/gViewCC.def Objects/gToolCC.def \
-                Objects/gPageCC.def Objects/gPenICC.def \
-                Objects/gGlyphC.def Objects/gTrigC.def \
-                Objects/gBoolGC.def Objects/gItemGC.def \
-                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
-                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
-                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
-                Objects/gValueC.def Objects/gToolGC.def \
-                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
-                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
-                math.def impex.def ieCommon.def ansic.def \
-                Internal/dib.def Internal/xlatLib.def \
-                transCommonGlobal.def graphicsMacro.def pcxGlobal.def \
-                pcx.def libExport.rdef libFormat.asm libMain.asm \
-                transUI.asm transLibEntry.asm transSemaphore.asm
 dib2pcx.obj \
 dib2pcx.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
                 geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
                 heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
                 file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
                 Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
                 Ansi/string.h
 pcxsave.obj \
 pcxsave.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
                 geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
                 heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
                 file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
                 Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
                 Ansi/string.h CommonH/pcx.h
 pcx2dib.obj \
 pcx2dib.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
                 geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
                 heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
                 file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
                 Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
                 Ansi/string.h
 pcxload.obj \
 pcxload.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
                 geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
                 heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
                 file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
                 Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
-                Ansi/string.h CommonH/pcx.h
-dib2pcx.obj \
-dib2pcx.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
-                geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
-                heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
-                file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
-                Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
-                Ansi/string.h
-pcxsave.obj \
-pcxsave.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
-                geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
-                heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
-                file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
-                Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
-                Ansi/string.h CommonH/pcx.h
-pcx2dib.obj \
-pcx2dib.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
-                geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
-                heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
-                file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
-                Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
-                Ansi/string.h
-pcxload.obj \
-pcxload.eobj: \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsimem.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiport.h \
-                geos.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsiwin.h \
-                heap.h Ansi/stdlib.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/fileio.h \
-                file.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsierror.h \
-                Internal/xlatLib.h Ansi/stdio.h \
-                c:/SYHRA6/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
+                /home/frehwagen/workspace/pcgeos/Library/Trans/Graphics/Bitmap/GraphicsCommonH/hsidib.h \
                 Ansi/string.h CommonH/pcx.h
 libExport.rdef: generic.uih product.uih impex.uih
 
