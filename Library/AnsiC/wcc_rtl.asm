6eab383709adc3cbf9703f4965ed5dd1f9d861d8
Jirka Kunze <jirka.kunze@googlemail.com>
2019-12-26 17:40:42 +0100

    implementation for __U4M and __I4M added

--- Library/AnsiC/wcc_rtl.asm
+++ Library/AnsiC/wcc_rtl.asm
@@ -7,12 +7,52 @@ WCC_TEXT        SEGMENT BYTE PUBLIC 'CODE'
 	public __I4D
 	public __CHP
 
-	__U4M proc near
-		push bp
-		mov sp, bp
 
-		ret
+;
+; __U4M, __I4M
+;
+; in:
+;       (dx:ax) - 32bit arg1 (dx hi, ax lo)
+;       (cx:bx) - 32bit arg2 (cx hi, bx lo)
+; out:
+;       (dx:ax) - 32bit product
+;
+; reg use: bx,cx destroyed, all others preserved or contain result.
+;
+; hi(result) := lo(hi(arg1) * lo(arg2)) +
+;               lo(hi(arg2) * lo(arg1)) +
+;               hi(lo(arg1) * lo(arg2))
+; lo(result) := lo(lo(arg1) * lo(arg2))
+;
+
+	__U4M proc far
+	.fall_thru
 	__U4M endp
+
+	__I4M proc far
+	uses si
+	.enter
+
+	xchg	si,ax			; save lo1
+	xchg	ax,dx
+	test	ax,ax           ; skip mul if hi1==0
+	jz		nohi1
+	mul		bx              ; hi1 * lo2
+
+	nohi1:					; if we jumped here, ax==0 so the following swap works
+	jcxz	nohi2			; skip mul if hi2==0
+	xchg	cx, ax			; result <-> hi2
+	mul		si				; lo1 * hi2
+	add		ax, cx			; ax = hi1*lo2 + hi2*lo1
+
+	nohi2:
+	xchg	ax,si
+	mul		bx				; lo1 * lo2
+	add		dx,si			; hi order result += partials
+          
+	.leave
+	ret
+	__I4M endp
 
 	__U4D proc near
 		push bp
@@ -21,13 +61,6 @@ WCC_TEXT        SEGMENT BYTE PUBLIC 'CODE'
 		ret
 	__U4D endp
 	
-	__I4M proc near
-		push bp
-		mov sp, bp
-
-		ret
-	__I4M endp
-
 	__I4D proc near
 		push bp
 		mov sp, bp
@@ -41,5 +74,6 @@ WCC_TEXT        SEGMENT BYTE PUBLIC 'CODE'
 
 		ret
 	__CHP endp	
+
 WCC_TEXT        ENDS
 
