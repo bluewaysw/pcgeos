ad37d16937883fde285b34955d0d22a10d500891
Falk Rehwagen <frehwagen@bluewaysw.de>
2020-11-07 22:24:59 +0100

    Fixed swat stub building for windows (using Strawberry Perl).

--- Tools/swat/Stub/local.mk
+++ Tools/swat/Stub/local.mk
@@ -61,7 +61,7 @@ NO_EC		= si, senor
 # Default to -DLOWMEM if target not in a subdir (e.g. dependencies.mk)
 #
 ASMFLAGS        := -i $(INCDIRS:S|^|-I|g) $(ASMFLAGS:N*.INCLUDES*) \
-		-D`perl -e 'print uc "$(SUBDIRS)"'`
+		-D`perl -e "print uc '$(SUBDIRS)'"`
 #		#-D`perl -e "$$_ = $$ARGV[0];" -e "s/(.*)\/.*/$$1/ or $$_ = \"LOWMEM\";" -e "print uc $$_;" $(.TARGET)` \
 #		#-D`expr $(.TARGET) : '\([^/]*\)/' \| LOWMEM : '\(LOWMEM\)' | tr a-z A-Z`
 
@@ -84,10 +84,10 @@ ASMFLAGS	+= -2 -DDO_DBCS -DPRODUCT=DOVE
 #
 # Define the targets and their sources...
 #
-STUBS		:= $(SUBDIRS:S|$|/swat.exe|g) 
+STUBS		:= $(SUBDIRS:S|$|/swat.exe|g)
 all		: $(STUBS)
 
-$(STUBS)	: $(OBJS:S|^|$(.TARGET:H)/|g)  	    	    LINK 
+$(STUBS)	: $(OBJS:S|^|$(.TARGET:H)/|g)  	    	    LINK
 
 $(SUBDIRS)	: $(.TARGET:S|$|/swat.exe|)
 
@@ -121,4 +121,3 @@ EXTRADEP	: .USE
 	rm -f dep.foo.$$$$
 
 $(DEPFILE)	: EXTRADEP
-
