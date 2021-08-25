d77d1a52ca50ec23d48e754722f4fbff4a26e7b0
Falk Rehwagen <rehwagen@exb.de>
2021-08-25 19:07:51 +0200

    Initial add of font driver for true type font renderings.

---
+++ Driver/Font/TrueType/local.mk
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# 	Copyright (c) GeoWorks 1992 -- All Rights Reserved
+#
+# PROJECT:	PC/GEOS
+# MODULE:	TrueType Driver -- special definitions
+# FILE: 	local.mk
+# AUTHOR: 	Gene Anderson
+#
+# DESCRIPTION:
+#	Special definitions required for the TrueType font driver
+#
+#	$Id: local.mk,v 1.1 97/04/18 11:45:28 newdeal Exp $
+#
+###############################################################################
+ASMFLAGS	+= -i
+
+.PATH.asm .PATH.def: ../FontCom $(INSTALL_DIR:H)/FontCom \
+
+#include	<$(SYSMAKEFILE)>
