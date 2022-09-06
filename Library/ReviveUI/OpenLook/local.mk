ebf3d685f4c1e63332bbf3deed5c056e5ea656e4
Falk Rehwagen <frehwagen@bluewaysw.de>
2022-09-06 06:33:50 +0200

    Update OpenLook local.mk to fix building issues. #173

--- Library/ReviveUI/OpenLook/local.mk
+++ Library/ReviveUI/OpenLook/local.mk
@@ -24,33 +24,6 @@
 # Another geode that bucks conventions -- we need to change the GEODE variable
 GEODE		= ol
 
-#
-#	Rules for updating ollib.def
-#
-
-MERGE_FILES	= copenButton.asm copenCtrl.asm copenReplyBar.asm \
-		  copenMenuButton.asm copenTrace.asm \
-		  copenApplication.asm copenSystem.asm\
-		  copenGauge.asm copenGlyphDisplay.asm \
-		  copenTextDisplay.asm copenTextEdit.asm\
-		  copenSettingCtrl.asm copenSetting.asm \
-		  cwinClass.asm cwinBase.asm cwinMenu.asm\
-		  cwinField.asm cwinScreen.asm cwinPopup.asm cwinDialog.asm \
-		  cwinWinIcon.asm cwinGlyphDisplay.asm\
-		  cviewPane.asm cviewPort.asm cviewPortGroup.asm\
-		  cviewSimplePane.asm cviewPortWindow.asm cviewScrollbar.asm\
-		  copenScrollList.asm copenScrollItem.asm copenCheckbox.asm\
-		  copenMenuItemGroup.asm cwinDisplayControl.asm\
-		  cwinDisplay.asm cwinMenuedWin.asm copenMenuBar.asm\
-		  copenTriggerBar.asm\
-		  copenGadget.asm copenGadgetComp.asm copenContent.asm\
-		  copenIsoContent.asm copenSpinGadget.asm copenRange.asm\
-		  copenDynamicList.asm cmainUIDocumentControl.asm \
-		  cmainAppDocumentControl.asm \
-		  cmainDocument.asm copenFileSelector.asm
-
-LIBHDR		= ollib.def
-
 PROTOCONST	= SPUI
 
 #
