354eb988102c0ebab03b0898decc252ff4f523c4
Jirka Kunze <jirkakunze@googlemail.com>
2023-09-09 18:47:05 +0200

    Medium is a synonym for Regular in some fonts.

--- Driver/Font/TrueType/Adapter/ttinit.c
+++ Driver/Font/TrueType/Adapter/ttinit.c
@@ -603,7 +603,7 @@ static AdjustedWeight mapFontWeight( TT_Short weightClass )
 
 static TextStyle mapTextStyle( const char* subfamily )
 {
-        if ( strcmp( subfamily, "Regular" ) == 0 )
+        if ( strcmp( subfamily, "Regular" ) == 0 || strcmp( subfamily, "Medium" ) == 0 )
                 return 0x00;
         if ( strcmp( subfamily, "Bold" ) == 0 )
                 return TS_BOLD;
