5d3658d9f007c3adb0e0d68416ec77f1798cd157
Jirka Kunze <jirkakunze@googlemail.com>
2023-10-04 20:11:35 +0200

    First step implement TrueType_Gen_Path.

--- Driver/Font/TrueType/Adapter/ttpath.c
+++ Driver/Font/TrueType/Adapter/ttpath.c
@@ -23,15 +23,21 @@
 /********************************************************************
  *                      TrueType_Gen_Path
  ********************************************************************
- * SYNOPSIS:	  
+ * SYNOPSIS:	  Draw outline of the given charcode to gstate.
  * 
- * PARAMETERS:    
+ * PARAMETERS:    gstate                GStateHande in which the outline 
+ *                                      of the character is written.
+ *                pathFlags             FGPF_POSTSCRIPT - transform for 
+ *                                           use as Postscript Type 1 or Type 3 font.
+ *			                FGPF_SAVE_STATE - do save/restore for GState
+ *                character             Character to build (GEOS Char).
+ *                pointSize             Desired point size.
+ *                *fontInfo             Pointer to FontInfo structure.
+ *                *outlineEntry         Ptr. to outline entry containing 
+ *                                      TrueTypeOutlineEntry.
+ *                varBlock              Memory handle to var block.
  * 
- * RETURNS:       
- * 
- * SIDE EFFECTS:  none
- * 
- * CONDITION:     
+ * RETURNS:       void 
  * 
  * STRATEGY:      
  * 
@@ -50,12 +56,52 @@ void _pascal TrueType_Gen_Path(
                         const OutlineEntry*  outlineEntry,
                         MemHandle            varBlock )
 {
+        TrueTypeVars*          trueTypeVars;
+        TrueTypeOutlineEntry*  trueTypeOutline;
+        TT_UShort              charIndex;
+
 
 EC(     ECCheckGStateHandle( gstate ) );
 EC(     ECCheckBounds( (void*)fontInfo ) );
 EC(     ECCheckBounds( (void*)outlineEntry ) );
 EC(     ECCheckMemHandle( varBlock ) );
 
+        /* get trueTypeVar block */
+        trueTypeVars = MemLock( varBlock );
+EC(     ECCheckBounds( (void*)trueTypeVars ) );
+
+        /* open face, create instance and glyph */
+        if( TrueType_Lock_Face(trueTypeVars, trueTypeOutline) )
+                goto Fail;
+
+        TT_New_Glyph( FACE, &GLYPH );
+
+         /* get TT char index */
+        charIndex = TT_Char_Index( CHAR_MAP, GeosCharToUnicode( character ) );
+
+        /* set pointsize and get metrics */
+        TT_Set_Instance_CharSize( INSTANCE, ( pointSize >> 10 ) );
+
+        /* load glyph and load glyphs outline */
+        TT_Load_Glyph( INSTANCE, GLYPH, charIndex, TTLOAD_DEFAULT );
+        TT_Get_Glyph_Outline( GLYPH, &OUTLINE );
+
+        /* write prologue */
+        if( pathFlags & FGPF_SAVE_STATE )
+                GrSaveState( gstate );
+
+
+        //TODO
+
+
+        /* write epilogue */
+        if( pathFlags & FGPF_SAVE_STATE )
+                GrRestoreState( gstate );
+
+        TT_Done_Glyph( GLYPH );
+
+Fail:
+        MemUnlock( varBlock );
 }
 
 
