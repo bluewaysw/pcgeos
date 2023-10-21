15a982a93939f191bd1e5743539a2f2f13cc1250
RainerB <r.bettsteller@web.de>
2023-10-21 12:20:49 +0200

    Revision history updated for the changes made.

--- Appl/GeoFile/local.mk
+++ Appl/GeoFile/local.mk
@@ -3,6 +3,11 @@
 #
 #	$Id: local.mk,v 1.1 97/04/04 15:54:28 newdeal Exp $
 #
+# REVISION HISTORY:
+#	Name	Date		Description
+#	----	----		-----------
+#				Initial version.
+#	RainerB	23.10.21	XCOMPFLAGS -zc flag added
 
 #
 # Define the Bullet specific version
