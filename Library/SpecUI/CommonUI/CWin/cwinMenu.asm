b82a1c318938946c4e64ced2364b52ebbe005f08
HubertHuckevoll <konstantin.meyer@gmail.com>
2024-02-11 22:26:06 +0100

    remove imagined potential bugfix in cwinMenu.asm / MenuWinScrollerScrollOnly - what was I thinking?

--- Library/SpecUI/CommonUI/CWin/cwinMenu.asm
+++ Library/SpecUI/CommonUI/CWin/cwinMenu.asm
@@ -5021,12 +5021,10 @@ atBottom:
 doneTaskBar:
 	pop	ds
 	push	bx, ax					; save top, bottom
-	jmp	afterTaskbar
 endIfTaskbar:
 else
 	push	bx, dx					; save top, bottom
 endif
-afterTaskbar:
 	call	VisQueryWindow				; di = window handle
 	tst	di
 	jz	update
