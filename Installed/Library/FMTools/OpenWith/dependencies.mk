5f14b92e93eda47b2708ce8a67f76c7f0964521d
HubertHuckevoll <konstantin.meyer@gmail.com>
2024-04-17 21:30:19 +0200

    add code, apparently a lot more work than expected is needed

--- Installed/Library/FMTools/OpenWith/dependencies.mk
+++ Installed/Library/FMTools/OpenWith/dependencies.mk
@@ -25,7 +25,7 @@ openwith.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
                 disk.h drive.h input.h char.h hwr.h win.h uDialog.h \
                 Objects/gInterC.h Objects/Text/tCommon.h stylesh.h \
                 driver.h thread.h print.h Internal/spoolInt.h serialDr.h \
-                parallDr.h hugearr.h fileEnum.h Ansi/string.h \
-                Ansi/stdio.h
+                parallDr.h hugearr.h fileEnum.h initfile.h Ansi/stdio.h \
+                Ansi/stdlib.h Ansi/string.h
 
 openwithEC.geo openwith.geo : geos.ldf ui.ldf ansic.ldf 
