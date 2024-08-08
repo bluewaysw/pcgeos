348ada29296495e11b2a31a20a7e43fef9de8f9c
Jirka Kunze <jirka.kunze@googlemail.com>
2024-08-08 22:27:42 +0200

    Filling instances cvt table fixed.

--- Driver/Font/TrueType/FreeType/ttobjs.c
+++ Driver/Font/TrueType/FreeType/ttobjs.c
@@ -891,10 +891,10 @@
     /* Scale the cvt values to the new ppem.          */
     /* We use by default the y ppem to scale the CVT. */
 
-    MulDivList( ins->cvt,
-                ins->cvtSize,
-                ins->metrics.scale1,
-                ins->metrics.scale2 );
+    for ( i = 0; i < ins->cvtSize; ++i )
+      ins->cvt[i] = TT_MulDiv( face->cvt[i],
+                               ins->metrics.scale1,
+                               ins->metrics.scale2 );
 
     /* All twilight points are originally zero */
     for ( j = 0; j < ins->twilight.n_points; ++j )
