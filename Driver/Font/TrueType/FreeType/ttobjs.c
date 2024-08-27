3039cc3489426a0636f72e3bc76004410e8f7645
Jirka Kunze <jirka.kunze@googlemail.com>
2024-08-27 20:26:41 +0200

    Quick fix for problem in some vtt hinted fonts.

--- Driver/Font/TrueType/FreeType/ttobjs.c
+++ Driver/Font/TrueType/FreeType/ttobjs.c
@@ -889,7 +889,11 @@
 
     /* Scale the cvt values to the new ppem.          */
     /* We use by default the y ppem to scale the CVT. */
-    MulDivList( ins->cvt, ins->cvtSize, face->cvt, ins->metrics.scale1, ins->metrics.scale2 );
+    for ( i = 0; i < ins->cvtSize; ++i )
+      ins->cvt[i] = TT_MulDiv( face->cvt[i],
+                               ins->metrics.scale1,
+                               ins->metrics.scale2 );
+    //MulDivList( ins->cvt, ins->cvtSize, face->cvt, ins->metrics.scale1, ins->metrics.scale2 );
 
     /* All twilight points are originally zero */
     for ( i = 0; i < ins->twilight.n_points; ++i )
