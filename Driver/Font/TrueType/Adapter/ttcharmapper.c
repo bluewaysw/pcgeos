ce1b12ab3e99d21aebbb67970a0b360a04045ce1
Jirka Kunze <jirka.kunze@googlemail.com>
2024-10-01 20:21:21 +0200

    Infinity loop fixed.

--- Driver/Font/TrueType/Adapter/ttcharmapper.c
+++ Driver/Font/TrueType/Adapter/ttcharmapper.c
@@ -484,7 +484,7 @@ word  GetGEOSCharForIndex( LookupEntry* lookupTable, word index )
         int right = NUM_CHARMAPENTRIES - 1;
 
         while (left <= right) {
-                int mid = left + (right - left) >> 1;
+                int mid = left + ( (right - left) >> 1 );
                 if ( lookupTable[mid].ttindex == index )
                         return lookupTable[mid].geoscode; 
                 else if (lookupTable[mid].ttindex < index)
