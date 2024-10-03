34cbe85999f25d6dc5b5532c88ab702eca6c8c81
Konstantin Meyer <konstantin.meyer@gmail.com>
2024-10-03 12:31:40 +0200

    make blend color work with new colorSelectorAPI, UI mechanics still wonky

--- Appl/Breadbox/Graphvwr/ASMMOD/asmmodManager.asm
+++ Appl/Breadbox/Graphvwr/ASMMOD/asmmodManager.asm
@@ -1,10 +1,16 @@
 include stdapp.def
 
+include geos.def        ; standard macros
+include resource.def    ; idata/udata, ProcCallFixedOrMovable etc.
+include gstring.def
+
+
 EspCode segment
 
+; public routines defined in this module
 global MYVMCOPYVMCHAIN:far
 global MYVMFREEVMCHAIN:far
-
+global MYGRMAPCOLORINDEX:far
 
 SetGeosConvention
 
@@ -12,7 +18,7 @@ MYVMCOPYVMCHAIN  proc    far     sourceFile:word,
                                  sourceChainL:word,
                                  sourceChainH:word,
                                  destFile:word
-                          
+
 	.enter
 
         mov     dx, destFile
@@ -30,7 +36,7 @@ MYVMCOPYVMCHAIN  endp
 MYVMFREEVMCHAIN  proc    far     sourceFile:word,
                                  sourceChainL:word,
                                  sourceChainH:word
-                          
+
 	.enter
 
         mov     bx, sourceFile
@@ -43,6 +49,23 @@ MYVMFREEVMCHAIN  proc    far     sourceFile:word,
 	ret
 
 MYVMFREEVMCHAIN  endp
+
+MYGRMAPCOLORINDEX proc  far     _gstate:word,
+                                _col:word
+        uses    bx,di
+
+        .enter
+                mov di,_gstate
+                mov ah,{byte}_col
+                call GrMapColorIndex
+                mov ah,bl
+                mov dl,bh
+                mov dh,{byte}_col
+        .leave
+        ret
+
+MYGRMAPCOLORINDEX endp
+
 
 EspCode ends
 
