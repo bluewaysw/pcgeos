05757951cc1b03ecfe34d651caf8f8aaa1fcf7a2
Jirka Kunze <jirka.kunze@googlemail.com>
2024-12-16 20:27:52 +0100

    Furter bug in interpreter fixed.

--- Driver/Font/TrueType/FreeType/ttinterp.c
+++ Driver/Font/TrueType/FreeType/ttinterp.c
@@ -1437,8 +1437,8 @@
       else
       {
         CUR.func_freeProj = Free_Project;
-        CUR.F_dot_P = (Long)CUR.GS.projVector.x * ( CUR.GS.freeVector.x << 2 ) +
-                      (Long)CUR.GS.projVector.y * ( CUR.GS.freeVector.y << 2 );
+        CUR.F_dot_P = (Long)CUR.GS.projVector.x * CUR.GS.freeVector.x << 2 +
+                      (Long)CUR.GS.projVector.y * CUR.GS.freeVector.y << 2;
       }
     }
 
