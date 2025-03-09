4e29b50bcfd07bed179c4369b290e579450b969a
Falk Rehwagen <frehwagen@bluewaysw.de>
2025-03-09 12:47:14 +0100

    Add missing gopher comps (Navigate, GServer) #888

---
+++ Appl/GServer/local.mk
@@ -0,0 +1,24 @@
+##############################################################################
+#
+#       Copyright (c) Geoworks 1994 -- All Rights Reserved
+#
+# PROJECT:      Sample Gopher Server
+# MODULE:       Sample Gopher Server
+# FILE:         local.mk
+# AUTHOR:       acham, 12/21/94
+#
+# REVISION HISTORY:
+#       Name    Date            Description
+#       ----    ----            -----------
+#       acham   12/21/94        Initial Revision
+#
+# DESCRIPTION:
+#       Special definitions for GServer
+#
+#       $Id: local.mk,v 1.1 97/04/04 15:11:26 newdeal Exp $
+#
+##############################################################################
+
+LINKFLAGS += -r
+
+#include <$(SYSMAKEFILE)>
