e85bfab0af7afb892b583f9b882caa7c9a4c7f42
HubertHuckevoll <konstantin.meyer@gmail.com>
2025-03-14 21:39:01 +0100

    cleanup, formatting, error handling

--- Library/PngLib/pngexp.h
+++ Library/PngLib/pngexp.h
@@ -3,6 +3,9 @@
 
 #include "pnglib.h"
 
+// returns FALSE on error
+#define FILE_WRITE_CHECKED(file, buffer, size) (FileWrite((file), (buffer), (size), FALSE) == (size))
+
 // Chunk Writing Functions
 Boolean _pascal writePngHeader(FileHandle file);
 Boolean _pascal writeIHDRChunk(FileHandle file, pngIHDRData* ihdrData);
@@ -13,8 +16,8 @@ Boolean _pascal writeIENDChunk(FileHandle file);
 // Processing Functions
 dword _pascal calculateScanlineBufferSize(dword width, BMFormat bitform);
 word _pascal calcBytesPerPixel(pngIHDRData* ihdrData);
-void _pascal filterScanline(byte* lineptr, byte* scanlineBuffer, byte *prevLine, word scanlineSize, word bytesPerPixel);
-Boolean _pascal deflateScanline(void *scanlineBuffer, word scanlineSize, z_stream *zstrm, FileHandle file, dword *idatChunkSize, dword *crc);
+void _pascal filterScanline(byte* scanlinePtr, byte* filteredScanlinePtr, byte *prevScanlinePtr, word scanlineSize, word bytesPerPixel);
+Boolean _pascal deflateScanline(void *filteredScanlinePtr, word scanlineSize, z_stream *zstrm, FileHandle file, dword *idatChunkSize, dword *crc);
 
 // Utility Functions
 word _pascal mapGEOSToPNGColorType(BMType bmptype);
