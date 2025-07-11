5037c01780f098a7d3e2bd53e9ac4bb07f2a5163
RainerB <r.bettsteller@web.de>
2025-07-11 21:19:47 +0200

    RabeSoft Libraries initial version

---
+++ Library/RabeSoft/bmptools/local.mk
@@ -0,0 +1,21 @@
+#
+#	BMP-Tools Library by Rabe-Soft
+#	Rainer Bettsteller, Magdeburg, Germany
+#	Adapted for free PC/GEOS project 01/2024 - 07/2025
+#
+
+NO_EC = 1
+
+
+#include <$(SYSMAKEFILE)>
+
+# The manual says I should do this... ;-)
+XGOCFLAGS = -L bmptools
+
+
+# XCCOMFLAGS = -zc -zu
+
+# Create a Copyrigth-notice
+LINKFLAGS += -N "Made by RABE-Soft 01/2000-07/25"
+
+
