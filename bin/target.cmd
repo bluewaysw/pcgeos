eb6916fe87be00a5319646e7de5b25344237cf0e
mgroeber9110 <45620825+mgroeber9110@users.noreply.github.com>
2025-12-28 23:24:52 +0100

    Simplify parallel development with separate EC and NC target builds (#769)
    * Simplify parallel development with separate EC and NC target builds (#700)
    
    * Make pcsn script work with FreeGEOS product/filetree
    
    * Remove files that are just left-over config files from using buildbbx.pl script with machine-specific paths
    
    * target.cmd: use .bbxxip folder to find target instead of hard-coded names
    
    * Review fixes
    
    * Update Linux version of target launcher script to use new method
    
    * Fixes to Linux 'target' command from code review

--- bin/target.cmd
+++ bin/target.cmd
@@ -4,10 +4,24 @@ if "%~1"=="-fcc" (
    CALL <NUL %0 -fcc %*
    GOTO :EOF
 )
+if "%1"=="-n" (
+   set TYPE=nec
+) ELSE (
+   set TYPE=ec
+)
+IF NOT EXIST %LOCAL_ROOT%\.bbxxip\.bbxxip.nt.%TYPE% (
+   echo *** Target type "%TYPE%" not built.
+   GOTO :EOF
+)
+for /f "delims=" %%a in (%LOCAL_ROOT%\.bbxxip\.bbxxip.nt.%TYPE%) do set %%a
+IF NOT EXIST %destdir%\localpc\ensemble\ensemble.bat (
+   echo *** Target at "%destdir%" not usable.
+   GOTO :EOF
+)
 IF NOT DEFINED BASEBOX (SET BASEBOX=dosbox)
 set OLD_PATH=%cd%
-cd /D %LOCAL_ROOT%\gbuild\localpc 
-del /F %LOCAL_ROOT%\gbuild\localpc\IPX_STAT.txt
+cd /D %destdir%\localpc 
+del /F "%destdir%\localpc\IPX_STAT.txt"
 del /F ensemble\init.bat
 IF DEFINED GEOS_CENTRAL_STORAGE (
    echo mount s: %GEOS_CENTRAL_STORAGE% >> ensemble\init.bat
@@ -26,13 +40,13 @@ start /B %BASEBOX% -conf %ROOT_DIR%\bin\basebox.conf -conf %LOCAL_ROOT%\basebox_
 cd %OLD_PATH%
 @cls
 :waitForFile
-@IF EXIST %LOCAL_ROOT%\gbuild\localpc\IPX_STAT.txt GOTO foundFile
+@IF EXIST %destdir%\localpc\IPX_STAT.txt GOTO foundFile
 @sleep 1s
 @echo|set /p="."
 @GOTO waitForFile
 :foundFile
-FINDSTR /r /c:"127.0.0.1 from port" %LOCAL_ROOT%\gbuild\localpc\IPX_STAT.txt | perl -e "my $status = <>; $status =~  m/(\d+)$/; printf('%%04X', $1);" > %LOCAL_ROOT%\gbuild\localpc\IPX_PORT.txt
-set /p IPX_PORT=<%LOCAL_ROOT%\gbuild\localpc\IPX_PORT.txt
+FINDSTR /r /c:"127.0.0.1 from port" %destdir%\localpc\IPX_STAT.txt | perl -e "my $status = <>; $status =~  m/(\d+)$/; printf('%%04X', $1);" > %destdir%\localpc\IPX_PORT.txt
+set /p IPX_PORT=<%destdir%\localpc\IPX_PORT.txt
 cls
 rem mode 120,50
 IF EXIST "%USERPROFILE%\swat.rc" (
