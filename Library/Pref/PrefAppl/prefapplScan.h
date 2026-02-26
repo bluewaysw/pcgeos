fea22087559fc7d9c6dc60a3bb59e78c0039a67c
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-02-26 12:30:15 +0100

    check initial version of step2, nothing works.

---
+++ Library/Pref/PrefAppl/prefapplScan.h
@@ -0,0 +1,40 @@
+#ifndef _PREF_APPL_SCAN_H_
+#define _PREF_APPL_SCAN_H_
+
+#include <file.h>
+#include <geode.h>
+
+typedef enum {
+    PREF_APPL_SCAN_ERROR_NONE = 0,
+    PREF_APPL_SCAN_ERROR_DISTAPPL_MISSING = 1,
+    PREF_APPL_SCAN_ERROR_OUT_OF_MEMORY = 2
+} PrefApplScanError;
+
+typedef struct {
+    GeodeToken PAAR_token;
+    FileLongName PAAR_name;
+    Boolean PAAR_selected;
+} PrefApplApplicationRecord;
+
+typedef struct {
+    StandardPath PALR_sourceRoot;
+    DiskHandle PALR_targetDisk;
+    GeodeToken PALR_targetToken;
+    PathName PALR_relativeDir;
+    FileLongName PALR_linkName;
+    PathName PALR_targetPath;
+} PrefApplLinkRecord;
+
+Boolean PrefApplRescanApplicationLinks(void);
+void PrefApplClearScannedLinks(void);
+word PrefApplGetScannedLinkCount(void);
+Boolean PrefApplGetScannedLinkRecord(word index, PrefApplLinkRecord *record);
+Boolean PrefApplDidHitTraversalDepthLimit(void);
+
+Boolean PrefApplRefreshApplicationData(void);
+void PrefApplClearScannedApplications(void);
+word PrefApplGetApplicationCount(void);
+Boolean PrefApplGetApplicationRecord(word index, PrefApplApplicationRecord *record);
+PrefApplScanError PrefApplGetLastScanError(void);
+
+#endif
