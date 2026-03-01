c119291837ed33229beafda0ba809c4eeabfb69f
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-03-01 14:40:27 +0100

    tiny UI changes

--- Installed/Library/Pref/PrefAppl/dependencies.mk
+++ Installed/Library/Pref/PrefAppl/dependencies.mk
@@ -16,7 +16,9 @@ prefappl.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
                 Objects/gItemC.goh Objects/gBoolC.goh \
                 Objects/gGadgetC.goh Objects/gToolCC.goh \
                 Objects/gValueC.goh Objects/gToolGC.goh \
-                Objects/helpCC.goh config.goh token.h Art/moniker.goh
+                Objects/helpCC.goh config.goh token.h Art/moniker.goh \
+                Art/mkrRightArrowSC.goh Art/mkrRightArrowSM.goh \
+                Art/mkrRightArrowSCGA.goh
 prefappl.obj \
 prefappl.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
                 graphics.h fontID.h font.h color.h gstring.h timer.h vm.h \
