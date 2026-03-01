87710c6c1b987a02a1e1800054734ad0e1d03108
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-03-01 14:11:43 +0100

    initial implement step4: two list refactor

--- Library/Pref/PrefAppl/prefapplLow.h
+++ Library/Pref/PrefAppl/prefapplLow.h
@@ -40,7 +40,10 @@ void PrefApplRemoveScannedLinksForToken(const GeodeToken *token);
 Boolean PrefApplRefreshApplicationData(void);
 void PrefApplClearScannedApplications(void);
 word PrefApplGetApplicationCount(void);
+word PrefApplGetLinkedApplicationCount(void);
 Boolean PrefApplGetApplicationRecord(word index, PrefApplApplicationRecord *record);
+Boolean PrefApplMapLinkedIndexToApplicationIndex(word linkedIndex, word *appIndexPtr);
+Boolean PrefApplMapApplicationIndexToLinkedIndex(word appIndex, word *linkedIndexPtr);
 Boolean PrefApplSetApplicationSelected(word index, Boolean selected);
 PrefApplError PrefApplGetLastScanError(void);
 
