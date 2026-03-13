b64a706ea02092290b5d4b8dfc31cf3a3602702b
HubertHuckevoll <konstantin.meyer@gmail.com>
2026-03-13 22:12:01 +0100

    check in original Makefile for MegaFile

---
+++ Installed/Driver/IFS/GEOS/MegaFile/dependencies.mk
@@ -0,0 +1,20 @@
+megafileManager.obj \
+megafileManager.eobj: gfsGeode.def geos.def heap.def geode.def resource.def \
+                ec.def lmem.def system.def localize.def sllang.def \
+                drive.def disk.def file.def driver.def timedate.def \
+                sem.def timer.def initfile.def gcnlist.def \
+                Objects/metaC.def object.def chunkarr.def geoworks.def \
+                graphics.def fontID.def font.def color.def \
+                Internal/semInt.def Internal/interrup.def \
+                Internal/dos.def fileEnum.def Internal/fileInt.def \
+                Internal/driveInt.def Internal/diskInt.def \
+                Internal/fsd.def Internal/log.def Internal/heapInt.def \
+                sysstats.def Internal/geodeStr.def Internal/fileStr.def \
+                Internal/localInt.def char.def Internal/fsDriver.def \
+                Internal/dosFSDr.def gfsConstant.def Internal/mfsDr.def \
+                megafileConstant.def gfsVariable.def megafileVariable.def \
+                gfsDisk.asm gfsEntry.asm gfsEnum.asm gfsExtAttrs.asm \
+                gfsInitExit.asm gfsIO.asm gfsMapPath.asm gfsPath.asm \
+                gfsUtils.asm megafileDevSpec.asm
+
+megafileEC.geo megafile.geo : geos.ldf 
