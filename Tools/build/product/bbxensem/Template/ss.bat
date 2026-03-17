5b7f73c8fbdccc96797a9aa65e68d7fc6eb2a870
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-03-17 21:39:35 +0100

    remove /sp_top from ss.bat

--- Tools/build/product/bbxensem/Template/ss.bat
+++ Tools/build/product/bbxensem/Template/ss.bat
@@ -1,3 +1,4 @@
 rem Run VAR(productlongname) with Swat debugger
 rem    (pauses for swat)
-swat /s EC-dos(loader.exe) /sp_top:C:\localpc\geos %1 %2 %3
+rem swat /s EC-dos(loader.exe) /sp_top:C:\localpc\geos %1 %2 %3
+swat /s EC-dos(loader.exe) %1 %2 %3 %4
