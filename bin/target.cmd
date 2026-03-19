d48dce283d3666d7182cf53a19231197cd372094
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-03-19 20:31:56 +0100

    make localpc drivec again

--- bin/target.cmd
+++ bin/target.cmd
@@ -14,31 +14,27 @@ IF NOT EXIST %LOCAL_ROOT%\.bbxxip\.bbxxip.nt.%TYPE% (
    GOTO :EOF
 )
 for /f "delims=" %%a in (%LOCAL_ROOT%\.bbxxip\.bbxxip.nt.%TYPE%) do set %%a
-set GEOS_ROOT_REL=localpc\geos
-IF NOT EXIST %destdir%\%GEOS_ROOT_REL%\ensemble.bat (
+IF NOT EXIST %destdir%\localpc\geos\ensemble.bat (
    echo *** Target at "%destdir%" not usable.
    GOTO :EOF
 )
 IF NOT DEFINED BASEBOX (SET BASEBOX=dosbox)
 set OLD_PATH=%cd%
-cd /D %destdir%
+cd /D %destdir%\localpc
 del /F "%destdir%\localpc\IPX_STAT.TXT"
-del /F %GEOS_ROOT_REL%\init.bat
+del /F geos\init.bat
 IF DEFINED GEOS_CENTRAL_STORAGE (
-   echo mount s: %GEOS_CENTRAL_STORAGE% >> %GEOS_ROOT_REL%\init.bat
+   echo mount s: %GEOS_CENTRAL_STORAGE% >> geos\init.bat
 )
 IF DEFINED GEOS_CDROM_DRIVE (
    IF EXIST "%GEOS_CDROM_DRIVE%\" (
-      echo mount r %GEOS_CDROM_DRIVE% -t cdrom >> %GEOS_ROOT_REL%\init.bat
+      echo mount r %GEOS_CDROM_DRIVE% -t cdrom >> geos\init.bat
    ) ELSE (
-      echo imgmount r "%GEOS_CDROM_DRIVE%" -t iso >> %GEOS_ROOT_REL%\init.bat
+      echo imgmount r "%GEOS_CDROM_DRIVE%" -t iso >> geos\init.bat
    )
 )
-IF EXIST %GEOS_ROOT_REL%\init.bat (
-   echo swatgo >> %GEOS_ROOT_REL%\init.bat
-)
-IF EXIST %GEOS_ROOT_REL%\privdata\INI.BAK (
-   perl -pi -e "s{C:\\\\GEOS\\\\GFS\\.IMG}{C:\\\\IMAGE\\\\GFS.IMG}ig; s{C:\\\\GEOS\\\\NETEC\\.INI}{C:\\\\LOCALPC\\\\GEOS\\\\NETEC.INI}ig; s{C:\\\\GEOS\\\\NET\\.INI}{C:\\\\LOCALPC\\\\GEOS\\\\NET.INI}ig; s{bootstrapPath\\s*=\\s*C:\\\\GEOS\\b}{bootstrapPath = C:\\\\LOCALPC\\\\GEOS}ig;" %GEOS_ROOT_REL%\privdata\INI.BAK
+IF EXIST geos\init.bat (
+   echo swatgo >> geos\init.bat
 )
 start /B %BASEBOX% -conf %ROOT_DIR%\bin\basebox.conf -conf %LOCAL_ROOT%\basebox_user.conf -noconsole
 cd %OLD_PATH%
