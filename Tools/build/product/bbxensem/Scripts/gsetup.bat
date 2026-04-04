c778fc8552d83b811695a2e5059af9f8a0ce09ed
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-04-04 21:31:49 +0200

    rename enable to activate

--- Tools/build/product/bbxensem/Scripts/gsetup.bat
+++ Tools/build/product/bbxensem/Scripts/gsetup.bat
@@ -1,28 +1,28 @@
 @echo off
 rem GSETUP.BAT
-rem Install/enable from current target directory.
+rem Install/activate from current target directory.
 rem
 rem Internal launcher contract:
 rem   GSETUP.BAT <GEOS_DIST_DIR> INSTALL
 rem   GSETUP.BAT <GEOS_DIST_DIR> INSTALL -F
-rem   GSETUP.BAT <GEOS_DIST_DIR> ENABLE
+rem   GSETUP.BAT <GEOS_DIST_DIR> ACTIVATE
 rem
 rem Usage:
 rem   GSETUP INSTALL
 rem   GSETUP INSTALL -F
-rem   GSETUP ENABLE
+rem   GSETUP ACTIVATE
 
 if "%1"=="" goto NOENTRY
 if "%2"=="" goto NOENTRY
 
 rem Keep variable footprint small for COMMAND.COM environment space limits.
 set PINST=setup\install
-set PENAB=setup\enable
+set PACT=setup\activate
 
 if "%2"=="install" goto PARSEINSTALL
 if "%2"=="INSTALL" goto PARSEINSTALL
-if "%2"=="enable" goto PARSEENABLE
-if "%2"=="ENABLE" goto PARSEENABLE
+if "%2"=="activate" goto PARSEACTIVATE
+if "%2"=="ACTIVATE" goto PARSEACTIVATE
 goto USAGE
 
 :PARSEINSTALL
@@ -43,7 +43,7 @@ if exist %1\%PINST%\NUL goto CHECKINSTALL3
 goto BADDIR
 
 :CHECKINSTALL3
-if exist %1\%PENAB%\NUL goto CHECKINI
+if exist %1\%PACT%\NUL goto CHECKINI
 goto BADDIR
 
 :CHECKINI
@@ -74,28 +74,28 @@ if exist %1\%PINST%\geosec.ini copy %1\%PINST%\geosec.ini .\
 if exist %1\%PINST%\geos.ini copy %1\%PINST%\geos.ini .\
 
 echo.
-echo Running enable phase ...
-goto DOENABLEINSTALL
+echo Running activate phase ...
+goto DOACTIVATEINSTALL
 
-:PARSEENABLE
+:PARSEACTIVATE
 if not "%3"=="" goto USAGE
-if exist %1\%PENAB%\NUL goto DOENABLE
+if exist %1\%PACT%\NUL goto DOACTIVATE
 goto BADDIR
 
-:DOENABLE
-echo Enabling from %1\%PENAB% to current target directory ...
-xcopy %1\%PENAB%\*.* .\ /S /E /Y
+:DOACTIVATE
+echo Activating from %1\%PACT% to current target directory ...
+xcopy %1\%PACT%\*.* .\ /S /E /Y
 echo.
 
 echo Regenerating GFS bootstrap INI for %1 ...
 call %1\writegfs.bat %1
-if exist gfsec.ini goto ENABLEDONE
-if exist gfs.ini goto ENABLEDONE
+if exist gfsec.ini goto ACTIVATEDONE
+if exist gfs.ini goto ACTIVATEDONE
 goto GFSGENFAIL
 
-:DOENABLEINSTALL
-echo Enabling from %1\%PENAB% to current target directory ...
-xcopy %1\%PENAB%\*.* .\ /S /E /Y
+:DOACTIVATEINSTALL
+echo Activating from %1\%PACT% to current target directory ...
+xcopy %1\%PACT%\*.* .\ /S /E /Y
 echo.
 
 echo Regenerating GFS bootstrap INI for %1 ...
@@ -104,8 +104,8 @@ if exist gfsec.ini goto INSTALLDONE
 if exist gfs.ini goto INSTALLDONE
 goto GFSGENFAIL
 
-:ENABLEDONE
-echo Enable complete.
+:ACTIVATEDONE
+echo Activate complete.
 goto END
 
 :INSTALLDONE
@@ -123,17 +123,17 @@ echo.
 echo Usage:
 echo   FREEGEOS\60BETA\GSETUP INSTALL
 echo   FREEGEOS\60BETA\GSETUP INSTALL -F
-echo   FREEGEOS\60BETA\GSETUP ENABLE
+echo   FREEGEOS\60BETA\GSETUP ACTIVATE
 goto END
 
 :BADDIR
 echo NOTICE: GSETUP must be called from target root directory.
-echo NOTICE: Current directory must contain FREEGEOS\60BETA\SETUP\INSTALL and FREEGEOS\60BETA\SETUP\ENABLE.
+echo NOTICE: Current directory must contain FREEGEOS\60BETA\SETUP\INSTALL and FREEGEOS\60BETA\SETUP\ACTIVATE.
 echo.
 echo Usage:
 echo   FREEGEOS\60BETA\GSETUP INSTALL
 echo   FREEGEOS\60BETA\GSETUP INSTALL -F
-echo   FREEGEOS\60BETA\GSETUP ENABLE
+echo   FREEGEOS\60BETA\GSETUP ACTIVATE
 goto END
 
 :USAGE
@@ -142,6 +142,6 @@ echo.
 echo Usage:
 echo   FREEGEOS\60BETA\GSETUP INSTALL
 echo   FREEGEOS\60BETA\GSETUP INSTALL -F
-echo   FREEGEOS\60BETA\GSETUP ENABLE
+echo   FREEGEOS\60BETA\GSETUP ACTIVATE
 
 :END
