c8f1fbb39cd1209323562363324613d45da3b73e
Konstantin Meyer <konstantin.meyer@gmail.com>
2026-04-18 12:16:12 +0200

    optimize writegfs.bat

--- Tools/build/product/bbxensem/Scripts/writegfs.bat
+++ Tools/build/product/bbxensem/Scripts/writegfs.bat
@@ -1,115 +1,55 @@
 @echo off
 rem WRITEGFS.BAT
 rem Generate GFSEC.INI or GFS.INI from GEOS_DIST_DIR argument.
+rem bootstrapPath holds the path with the
+rem GFS image (%1\BOOT). bootstrapPath is used to remove that
+rem path from the standard path list once the GFS has been loaded
+rem and runs.
 
 if "%1"=="" goto NOVAR
 
-if exist geosec.ini goto MAKEGFSEC
-if exist geos.ini goto MAKEGFS
-if exist %1\BOOT\NETEC.INI goto MAKEGFSEC
-goto MAKEGFS
+set GFS_INI=
+set BOOT_INI=
 
-:MAKEGFSEC
-if exist gfsec.ini del gfsec.ini
-if exist %1\BOOT\NETEC.INI goto GFSECNETEC
-if exist %1\BOOT\NET.INI goto GFSECNET
-goto GFSECNET
+if exist geosec.ini set GFS_INI=gfsec.ini
+if "%GFS_INI%"=="" if exist geos.ini set GFS_INI=gfs.ini
+if "%GFS_INI%"=="" goto NOCFG
 
-:GFSECNETEC
->gfsec.ini echo [system]
->>gfsec.ini echo fs = megafile.geo
->>gfsec.ini echo.
->>gfsec.ini echo [paths]
->>gfsec.ini echo top = %1\BOOT GFS:\FG
->>gfsec.ini echo ini = %1\BOOT\NETEC.INI
->>gfsec.ini echo.
->>gfsec.ini echo [gfs]
->>gfsec.ini echo file = %1\BOOT\GFS.IMG
->>gfsec.ini echo drive = GFS
->>gfsec.ini echo ; bootstrapPath holds the path with the
->>gfsec.ini echo ; GFS image. That path is GEOS_DIST_DIR\BOOT.
->>gfsec.ini echo ; bootstrapPath is used to remove that path from the
->>gfsec.ini echo ; standard path list once the GFS has been loaded and runs.
->>gfsec.ini echo bootstrapPath = %1\BOOT
->>gfsec.ini echo cacheFile = none
->>gfsec.ini echo.
-if exist gfsec.ini goto END
-echo ERROR: Failed to generate GFSEC.INI.
-goto END
+if "%GFS_INI%"=="gfsec.ini" set BOOT_INI=%1\BOOT\NETEC.INI
+if "%GFS_INI%"=="gfs.ini" set BOOT_INI=%1\BOOT\NET.INI
+if not exist %BOOT_INI% goto NONETCFG
 
-:GFSECNET
->gfsec.ini echo [system]
->>gfsec.ini echo fs = megafile.geo
->>gfsec.ini echo.
->>gfsec.ini echo [paths]
->>gfsec.ini echo top = %1\BOOT GFS:\FG
->>gfsec.ini echo ini = %1\BOOT\NET.INI
->>gfsec.ini echo.
->>gfsec.ini echo [gfs]
->>gfsec.ini echo file = %1\BOOT\GFS.IMG
->>gfsec.ini echo drive = GFS
->>gfsec.ini echo ; bootstrapPath holds the path with the
->>gfsec.ini echo ; GFS image. That path is GEOS_DIST_DIR\BOOT.
->>gfsec.ini echo ; bootstrapPath is used to remove that path from the
->>gfsec.ini echo ; standard path list once the GFS has been loaded and runs.
->>gfsec.ini echo bootstrapPath = %1\BOOT
->>gfsec.ini echo cacheFile = none
->>gfsec.ini echo.
-if exist gfsec.ini goto END
-echo ERROR: Failed to generate GFSEC.INI.
-goto END
+:WRITEINI
+if exist %GFS_INI% del %GFS_INI%
 
-:MAKEGFS
-if exist gfs.ini del gfs.ini
-if exist %1\BOOT\NET.INI goto GFSNET
-if exist %1\BOOT\NETEC.INI goto GFSNETEC
-goto GFSNET
-
-:GFSNET
->gfs.ini echo [system]
->>gfs.ini echo fs = megafile.geo
->>gfs.ini echo.
->>gfs.ini echo [paths]
->>gfs.ini echo top = %1\BOOT GFS:\FG
->>gfs.ini echo ini = %1\BOOT\NET.INI
->>gfs.ini echo.
->>gfs.ini echo [gfs]
->>gfs.ini echo file = %1\BOOT\GFS.IMG
->>gfs.ini echo drive = GFS
->>gfs.ini echo ; bootstrapPath holds the path with the
->>gfs.ini echo ; GFS image. That path is GEOS_DIST_DIR\BOOT.
->>gfs.ini echo ; bootstrapPath is used to remove that path from the
->>gfs.ini echo ; standard path list once the GFS has been loaded and runs.
->>gfs.ini echo bootstrapPath = %1\BOOT
->>gfs.ini echo cacheFile = none
->>gfs.ini echo.
-if exist gfs.ini goto END
-echo ERROR: Failed to generate GFS.INI.
-goto END
-
-:GFSNETEC
->gfs.ini echo [system]
->>gfs.ini echo fs = megafile.geo
->>gfs.ini echo.
->>gfs.ini echo [paths]
->>gfs.ini echo top = %1\BOOT GFS:\FG
->>gfs.ini echo ini = %1\BOOT\NETEC.INI
->>gfs.ini echo.
->>gfs.ini echo [gfs]
->>gfs.ini echo file = %1\BOOT\GFS.IMG
->>gfs.ini echo drive = GFS
->>gfs.ini echo ; bootstrapPath holds the path with the
->>gfs.ini echo ; GFS image. That path is GEOS_DIST_DIR\BOOT.
->>gfs.ini echo ; bootstrapPath is used to remove that path from the
->>gfs.ini echo ; standard path list once the GFS has been loaded and runs.
->>gfs.ini echo bootstrapPath = %1\BOOT
->>gfs.ini echo cacheFile = none
->>gfs.ini echo.
-if exist gfs.ini goto END
-echo ERROR: Failed to generate GFS.INI.
+>%GFS_INI% echo [system]
+>>%GFS_INI% echo fs = megafile.geo
+>>%GFS_INI% echo.
+>>%GFS_INI% echo [paths]
+>>%GFS_INI% echo top = %1\BOOT GFS:\FG
+>>%GFS_INI% echo ini = %BOOT_INI%
+>>%GFS_INI% echo.
+>>%GFS_INI% echo [gfs]
+>>%GFS_INI% echo file = %1\BOOT\GFS.IMG
+>>%GFS_INI% echo drive = GFS
+>>%GFS_INI% echo bootstrapPath = %1\BOOT
+>>%GFS_INI% echo cacheFile = none
+>>%GFS_INI% echo.
+if exist %GFS_INI% goto END
+echo ERROR: Failed to generate %GFS_INI%.
 goto END
 
 :NOVAR
 echo ERROR: GEOS_DIST_DIR argument is missing.
+goto END
+
+:NOCFG
+echo ERROR: Neither GEOSEC.INI nor GEOS.INI exists in current directory.
+goto END
+
+:NONETCFG
+echo ERROR: Missing required BOOT INI file: %BOOT_INI%
 
 :END
+set GFS_INI=
+set BOOT_INI=
