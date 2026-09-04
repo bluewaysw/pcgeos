f401aa8e36c123372e712d11c96934aad1596d93
HubertHuckevoll <konstantin.meyer@gmail.com>
2026-09-04 20:43:14 +0200

    Separate Html4Par and BbxBrow build variants

--- Library/Breadbox/Html4Par/local.mk
+++ Library/Breadbox/Html4Par/local.mk
@@ -1,32 +1,33 @@
+#
+# mkmf finds product directories by their is_a_product marker and records
+# them in the generated Makefile's PRODUCTS variable. Keep that list
+# available for explicit builds, but clear it before geode.mk so the
+# products do not become dependencies of the default full target.
+# The default is regular SBCS NC and EC; command-line PRODUCTS overrides it,
+# for example: pmake "PRODUCTS=JS" full
+#
+PRODUCTS =
+
 #include <$(SYSMAKEFILE)>
 
-# The manual says I should do this... ;-)
-XGOCFLAGS = -L html4par
-
+# Product feature matrix:
+#   regular       SBCS, neither JavaScript nor AutoBrowse
+#   DBCS          DBCS character mode only
+#   JS/JSDBCS     JAVASCRIPT_SUPPORT (plus DBCS for JSDBCS)
+#   AB/ABDBCS     COMPILE_OPTION_AUTO_BROWSE (plus DBCS for ABDBCS)
+# Html4Par derives HTML_SCRIPT_SUPPORT when either JavaScript or AutoBrowse
+# is enabled. Keep caller-supplied COMPILE_OPTIONS in a private variable:
+# pmake command-line variables cannot be appended to by a makefile.
+# All legacy variants can be built explicitly, for example:
+#   pmake "PRODUCTS=DBCS JS JSDBCS AB ABDBCS" full
+#
 COMPILE_OPTIONS ?=
-COMPILE_OPTIONS += $(.TARGET:X\\[JS\\]/*:S|JS| -DJAVASCRIPT_SUPPORT |g)
-COMPILE_OPTIONS += $(.TARGET:X\\[JSDBCS\\]/*:S|JSDBCS| -DJAVASCRIPT_SUPPORT |g)
+_HTML4PAR_OPTIONS := $(COMPILE_OPTIONS)
+_HTML4PAR_OPTIONS += $(.TARGET:X\\[JS\\]/*:S|JS| -DJAVASCRIPT_SUPPORT |g)
+_HTML4PAR_OPTIONS += $(.TARGET:X\\[JSDBCS\\]/*:S|JSDBCS| -DJAVASCRIPT_SUPPORT |g)
+_HTML4PAR_OPTIONS += $(.TARGET:X\\[AB\\]/*:S|AB| -DCOMPILE_OPTION_AUTO_BROWSE |g)
+_HTML4PAR_OPTIONS += $(.TARGET:X\\[ABDBCS\\]/*:S|ABDBCS| -DCOMPILE_OPTION_AUTO_BROWSE |g)
 
-XGOCFLAGS += $(COMPILE_OPTIONS)
-GOCFLAGS += $(COMPILE_OPTIONS)
-
-# -WDE: Make sure that SS!=DS situation in library is observed
-# -d:   Merge duplicate strings
-# -O:   Some other optimizations...
-# -Z:   Suppress register reloads
-# JavaScript code uses #if rather than #ifdef
-#XCCOMFLAGS = -d -O -Z -WDE $(COMPILE_OPTIONS:S|JAVASCRIPT_SUPPORT|JAVASCRIPT_SUPPORT=1|g)
-XCCOMFLAGS = -zu $(COMPILE_OPTIONS:S|JAVASCRIPT_SUPPORT|JAVASCRIPT_SUPPORT=1|g)
-#if $(PRODUCT) != "NDO2000"
-# -dc: Breaks mkmf...  Removed from NDO2000 build.
-##XCCOMFLAGS += -dc 
-#endif
-
-# -N:  Add stack probes to every routine (only for EC builds)
-#ifndef NO_EC
-#  XCCOMFLAGS += -N
-#endif
-
-# Set Copyright notice
-//XLINKFLAGS = -N \(C\)98\20Breadbox\20Computer\20Company $(COMPILE_OPTIONS)
-XLINKFLAGS = $(COMPILE_OPTIONS)
+GOCFLAGS += $(_HTML4PAR_OPTIONS)
+CCOMFLAGS += $(_HTML4PAR_OPTIONS:S|JAVASCRIPT_SUPPORT|JAVASCRIPT_SUPPORT=1|g)
+LINKFLAGS += $(_HTML4PAR_OPTIONS)
